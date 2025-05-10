cf4923867cfede262bda913ca6efc7fa5a5ead70
TahaZahid05 <tz09220@st.habib.edu.pk>
2025-05-11 04:25:31 +0500

    Made getHumanChoice(x) and prompted user for input

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+
+function getHumanChoice(x){
+    if (x == 0){
+        return "rock";
+    }
+    else if(x == 1){
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+let input = prompt("Enter a number between 0 and 2 for your input: ")
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(getHumanChoice(input))
